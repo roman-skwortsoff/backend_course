a8e8bbade0699151398fb5323e54235699b5bbb5
Roman Skvortsov <roman.skwortsoff@mail.ru>
2023-04-22 16:57:31 +0400

    add ILIKE filters

--- app/schemas/hotels.py
+++ app/schemas/hotels.py
@@ -1,1 +1,11 @@
-pass
+from pydantic import BaseModel, Field
+
+
+class Hotel(BaseModel):
+    title: str
+    location: str
+
+
+class HotelPATCH(BaseModel):
+    title: str | None = Field(None)
+    location: str | None = Field(None)
