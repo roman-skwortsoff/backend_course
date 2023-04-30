17581bc3b9e0d0199c1a31d60c99eb1fa59ca97b
Roman Skvortsov <roman.skwortsoff@mail.ru>
2023-04-30 11:57:01 +0400

    task_9

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -7,5 +7,5 @@ def register_exceptions(app):
     async def integrity_error_handler(request: Request, exc: IntegrityError):
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
-            content={"detail": "Duplicate email"},
+            content={"detail": "Пользователь с таким email уже существует"},
         )
--- app/repositories/users.py
+++ app/repositories/users.py
@@ -1,10 +1,18 @@
+from pydantic import EmailStr
+from sqlalchemy import select
 from app.models.users import UsersOrm
 from app.repositories.base import BaseReposirory
-from app.schemas.users import User
+from app.schemas.users import User, UseWithHashedPassword
 
 
 class UsersRepository(BaseReposirory):
     model = UsersOrm
     schema = User
+
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email = email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+        return UseWithHashedPassword.model_validate(model)
 
 
