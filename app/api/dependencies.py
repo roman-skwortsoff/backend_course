2675c2efe04e7a00a1938f8d49b668c839d826db
Roman Skvortsov <roman.skwortsoff@mail.ru>
2023-04-30 23:57:01 +0400

    task_10

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,12 +1,26 @@
 from typing import Annotated
-
-from fastapi import Depends, Query
+from fastapi import Depends, Query, Request, HTTPException
 from pydantic import BaseModel
+
+from app.services.auth import AuthService
 
 
 class PaginationParams(BaseModel):
     page: Annotated[int | None, Query(1, ge=1)]
     per_page: Annotated[int | None, Query(None, ge=1, lt=30)]
 
-
 PaginationDep = Annotated[PaginationParams, Depends()]
+
+
+def get_token(request: Request) -> str:
+    access_token = request.cookies.get('access_token', None)
+    if access_token is None:
+        raise HTTPException(status_code=401, detail="Вы не предоставили токен")
+    return access_token
+
+
+def get_current_user_id(token: str = Depends(get_token)) -> int:
+    data = AuthService().decode_token(token)
+    return data['user_id']
+
+UserIdDep = Annotated[id, Depends(get_current_user_id)]
