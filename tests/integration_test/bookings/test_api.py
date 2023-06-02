be0b85dea2fba61cbb40718296501923a106d914
Roman Skvortsov <roman.skwortsoff@mail.ru>
2023-06-02 14:57:01 +0400

    task 24

--- tests/integration_test/bookings/test_api.py
+++ tests/integration_test/bookings/test_api.py
@@ -1,4 +1,8 @@
 import pytest
+from sqlalchemy import delete
+
+from app.database import engine_null, Base
+from app.models import BookingsOrm
 
 
 @pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
@@ -28,3 +32,35 @@ async def test_add_booking(
         assert isinstance(res, dict)
         assert res["status"] == "OK"
         assert "booking" in res
+
+@pytest.fixture(scope="function")
+async def delete_all_bookings(db):
+    await db.session.execute(delete(BookingsOrm))
+    await db.commit()
+    yield db
+
+@pytest.mark.parametrize("room_id, date_from, date_to, count", [
+    (1, "2025-06-01", "2025-06-02", 1),
+    (1, "2025-06-01", "2025-06-02", 2),
+    (1, "2025-06-01", "2025-06-02", 3),
+])
+async def test_add_and_get_my_bookings(
+        room_id, date_from, date_to, count,
+        delete_all_bookings, authenticated_ac):
+    for _ in range(count):
+        response = await authenticated_ac.post(
+            "/bookings",
+            json={"room_id": room_id,
+                  "date_from": date_from,
+                  "date_to": date_to,
+                  }
+        )
+        assert response.status_code == 200
+
+    response = await authenticated_ac.get("/bookings/me")
+    data = response.json()
+    assert response.status_code == 200
+    assert isinstance(data, list)
+    assert len(data) == count
+
+
