eef4f847a0073f649fd1b4a025272a1bf9cd5878
Roman Skvortsov <roman.skwortsoff@mail.ru>
2023-06-02 08:57:01 +0400

    commit 02.06

--- app/repositories/bookings.py
+++ app/repositories/bookings.py
@@ -1,6 +1,5 @@
 from datetime import date
 
-from celery.worker.consumer.mingle import exception
 from fastapi import HTTPException
 from pydantic import BaseModel
 from sqlalchemy import select, insert
@@ -11,6 +10,7 @@ from app.models.bookings import BookingsOrm
 from app.repositories.base import BaseReposirory
 from app.repositories.mappers.mappers import BookingDataMapper, RoomDataMapper
 from app.repositories.utils import rooms_ids_for_booking
+from app.schemas.bookings import BookingAddData
 
 
 class BookingsRepository(BaseReposirory):
@@ -24,11 +24,12 @@ class BookingsRepository(BaseReposirory):
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
 
-    async def add_booking(self, data: BaseModel):
+    async def add_booking(self, data: BookingAddData, hotel_id:int):
 
         rooms_ids_to_get = rooms_ids_for_booking(
             date_from=data.date_from,
-            date_to=data.date_to
+            date_to=data.date_to,
+            hotel_id=hotel_id
         )
 
         # 1е решение
@@ -45,18 +46,16 @@ class BookingsRepository(BaseReposirory):
         # available_rooms = [RoomDataMapper.map_to_domain_entity(room) for room in result.scalars().all()]
         # available_rooms_ids = [room.id for room in available_rooms]
 
-        query = (
-            select(RoomsOrm.id)
-            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-        )
-        result = await self.session.execute(query)
+        # query = (
+        #     select(RoomsOrm.id)
+        #     .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        # )
+        result = await self.session.execute(rooms_ids_to_get)
         available_rooms_ids = [id for id in result.scalars().all()]
 
         if data.room_id in available_rooms_ids:
-            add_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
-            result = await self.session.execute(add_stmt)
-            model = result.scalars().one()
-            return self.mapper.map_to_domain_entity(model)
+            booking = await self.add(data)
+            return booking
         else:
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
