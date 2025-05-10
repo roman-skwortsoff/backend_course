a1b5e6beb67f0478e4d38dc357f2cf031c75afc4
roman-skwortsoff <roman.skwortsoff@mail.ru>
2025-05-10 13:20:18 +0400

    add facilities

---
+++ app/models/facilities.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+
+from app.database import Base
+
+
+class FacitiliesOrm(Base):
+    __tablename__ = "facilities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    
+
+
+class RoomsFacilitiesOrm(Base):
+    __tablename__ = "rooms_facilities"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    room_id = mapped_column(ForeignKey("rooms.id"))
+    facility_id= mapped_column(ForeignKey("facilities.id"))
