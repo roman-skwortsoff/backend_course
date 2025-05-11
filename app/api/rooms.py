fd793f0ca0c8520dc0f8c085895a0f70458656e6
Roman Skvortsov <roman.skwortsoff@mail.ru>
2025-05-11 18:44:12 +0400

    task 16 update

--- app/api/rooms.py
+++ app/api/rooms.py
@@ -54,21 +54,7 @@ async def put_room(hotel_id: int,
                    ) -> None:
     merged_data = RoomAddData(**room_data.model_dump(), hotel_id=hotel_id)
     await db.rooms.edit(merged_data, id=room_id)
-
-    facilities = await db.rooms_facilities.get_filtered(room_id=room_id)
-    facility_ids = {facility.facility_id for facility in facilities}
-    new_facility_ids = set(room_data.facilities_ids)
-
-    to_remove = facility_ids - new_facility_ids
-    to_add = new_facility_ids - facility_ids
-
-    if to_remove:
-        await db.rooms_facilities.delete_by_room_and_facilities(room_id=room_id, facility_ids=list(to_remove))
-
-    if to_add:
-        room_facilities_data = [RoomFacilityAdd(room_id=room_id, facility_id=f_id) for f_id in to_add]
-        await db.rooms_facilities.add_bulk(room_facilities_data)
-
+    await db.rooms_facilities.set_facilities_by_room(room_id=room_id, facility_ids=room_data.facilities_ids)
     await db.commit()
     return {"status": "OK"}
 
@@ -82,24 +68,12 @@ async def patch_room(hotel_id: int,
                      db: DBDep,
                      room_data: RoomPATCH = Body()
               ):
-    merged_data = RoomPatchData(**room_data.model_dump(exclude_unset=True), hotel_id=hotel_id)
+    room_data_dict = room_data.model_dump(exclude_unset=True)
+    merged_data = RoomPatchData(**room_data_dict, hotel_id=hotel_id)
     await db.rooms.edit(merged_data, is_patch=True, id=room_id)
 
-    if "facilities_ids" in room_data:
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        facilities = await db.rooms_facilities.get_filtered(room_id=room_id)
-        facility_ids = {facility.facility_id for facility in facilities}
-        new_facility_ids = set(room_data.facilities_ids)
-
-        to_remove = facility_ids - new_facility_ids
-        to_add = new_facility_ids - facility_ids
-
-        if to_remove:
-            await db.rooms_facilities.delete_by_room_and_facilities(room_id=room_id, facility_ids=list(to_remove))
-
-        if to_add:
-            room_facilities_data = [RoomFacilityAdd(room_id=room_id, facility_id=f_id) for f_id in to_add]
-            await db.rooms_facilities.add_bulk(room_facilities_data)
+    if 'facilities_ids' in room_data_dict:
+        await db.rooms_facilities.set_facilities_by_room(room_id=room_id, facility_ids=room_data_dict["facilities_ids"])
 
     await db.commit()
     return {"status": "OK"}
@@ -109,32 +83,3 @@ async def delete_room(hotel_id: int, room_id: int, db: DBDep) -> None:
     await db.rooms.delete(id=room_id, hotel_id=hotel_id)
     await db.commit()
     return {"status": "OK"}
-
-
-
-#
-# @router.patch("/{hotel_id}",
-#            summary="Частичное обновление данных об отеле",
-#            description="Тут мы частично обновляем данные, можно отправить name или title"
-#            )
-# async def patch_hotel(hotel_id: int, hotel_data: HotelPATCH
-#               ):
-#     async with async_session_maker() as session:
-#         await HotelRepository(session).edit(hotel_data, is_patch=True, id=hotel_id)
-#         await session.commit()
-#     return {"status": "OK"}
-#
-#
-# @router.delete("/{hotel_id}")
-# async def delete_hotel(hotel_id: int) -> None:
-#     async with async_session_maker() as session:
-#         await HotelRepository(session).delete(id=hotel_id)
-#         await session.commit()
-#     return {"status": "OK"}
-#
-# @router.get("/{hotel_id}")
-# async def get_hotel(hotel_id: int):
-#     async with async_session_maker() as session:
-#         hotel = await HotelRepository(session).get_one_or_none(id = hotel_id)
-#         await session.commit()
-#     return hotel
--- app/repositories/facilities.py
+++ app/repositories/facilities.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from sqlalchemy import insert, delete
+from sqlalchemy import insert, delete, select
 
 from app.repositories.base import BaseReposirory
 from app.models.facilities import FacilitiesOrm, RoomsFacilitiesOrm
@@ -15,10 +15,27 @@ class RoomsFacilitiesRepository(BaseReposirory):
     model = RoomsFacilitiesOrm
     schema = RoomFacility
 
-    async def delete_by_room_and_facilities(self,
-                                            room_id: int,
-                                            facility_ids: list[int]
-    ) -> None:
-        stmt = delete(RoomsFacilitiesOrm).where(RoomsFacilitiesOrm.room_id == room_id,
-                                           RoomsFacilitiesOrm.facility_id.in_(facility_ids))
-        await self.session.execute(stmt)
+    async def set_facilities_by_room(self,
+                                     room_id: int,
+                                     facility_ids: list[int]
+                                     ):
+        get_facilities_ids = (select(RoomsFacilitiesOrm.facility_id)
+                      .filter_by(room_id=room_id)
+                      )
+        res = await self.session.execute(get_facilities_ids)
+        current_facilities_ids = res.scalars().all()
+        ids_to_remove = list(set(current_facilities_ids) - set(facility_ids))
+        ids_to_add = list(set(facility_ids) - set(current_facilities_ids))
+
+        if ids_to_remove:
+            delete_stmt = (delete(RoomsFacilitiesOrm)
+                    .where(RoomsFacilitiesOrm.room_id == room_id,
+                                                    RoomsFacilitiesOrm.facility_id.in_(ids_to_remove))
+                    )
+            await self.session.execute(delete_stmt)
+
+        if ids_to_add:
+            insert_stmt = (insert(RoomsFacilitiesOrm)
+                    .values([{"room_id": room_id, "facility_id": f_id} for f_id in ids_to_add])
+                    )
+            await self.session.execute(insert_stmt)
