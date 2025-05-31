d4d57da2549215d85ca28f92adcfe151621c22e3
Roman Skvortsov <roman.skwortsoff@mail.ru>
2025-05-31 20:22:52 +0400

    update 31.05

--- tests/integration_test/facilities/test_api.py
+++ tests/integration_test/facilities/test_api.py
@@ -2,16 +2,16 @@
 
 
 async def test_get_facitities(ac):
-    responce = await ac.get(
+    response = await ac.get(
         "/facilities",
         params={}
     )
-    assert responce.status_code == 200
+    assert response.status_code == 200
 
 
 async def test_post_facitities(ac):
-    responce = await ac.post(
+    response = await ac.post(
         "/facilities",
         json={"title": "Джакузи"}
     )
-    assert responce.status_code == 200
+    assert response.status_code == 200
