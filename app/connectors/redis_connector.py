9a98c12f55f455651ede180be5d13eda7371dfcc
Romario <r.skwortsoff@gmail.com>
2025-06-20 19:27:07 +0400

    add docker compose

--- app/connectors/redis_connector.py
+++ app/connectors/redis_connector.py
@@ -1,18 +1,33 @@
 import logging
+import asyncio
 
 import redis.asyncio as redis
+from redis.exceptions import RedisError
 
 
 class RedisManager:
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, port: int, max_retries: int = 7, retry_delay: int = 2):
         self.host = host
         self.port = port
         self.redis = None
+        self.max_retries = max_retries
+        self.retry_delay = retry_delay
 
     async def connect(self):
-        logging.info(f"Начинаю подключение к Redis host={self.host}")
-        self.redis = await redis.Redis(host=self.host, port=self.port)
-        logging.info(f"Успешное подключение к Redis host={self.host}")
+        for attempt in range(1, self.max_retries + 1):
+            logging.info(f"[Redis] Попытка {attempt}/{self.max_retries} подключения к {self.host}:{self.port}")
+            self.redis = redis.Redis(host=self.host, port=self.port)
+            try:
+                pong = await self.redis.ping()
+                if pong:
+                    logging.info(f"[Redis] Успешное подключение к {self.host}:{self.port}")
+                    return
+            except RedisError as e:
+                logging.warning(f"[Redis] Ошибка подключения: {e}")
+            await asyncio.sleep(self.retry_delay)
+
+        logging.error(f"[Redis] Не удалось подключиться после {self.max_retries} попыток.")
+        raise ConnectionError("Подключение к Redis не удалось.")
 
     async def set(self, key: str, value: str, expire: int = None):
         if expire:
